60688e922f3d2e287e84926d80a6a07f6d441973
abujaipaul <abujaipaul1@gmail.com>
2025-10-11 03:21:55 +0100

    completed the counters

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ const cartIndicatorEmpty = document.querySelector("#cart_indicator_empty")
 const deleteButton = document.querySelector("#dustbin_delete")
 const substractButton = document.querySelector("#substract_button")
 const additionButton = document.querySelector("#addition_button")
-const itemPicked = document.querySelector("#item_picked")
+// variables tragetting the item picked span.
+const itemPicked = document.querySelectorAll("#item_picked")[2]
+const cartIconPicked = document.querySelectorAll("#item_picked")[0]
+const itemPickedInCart = document.querySelectorAll("#item_picked")[1]
 
 
  //to trigger the cart dropdown.
@@ -22,4 +25,26 @@ function deleteItems(){
  deleteButton.addEventListener("click", deleteItems)
 
  //to add or minus number of goods picked.
+let numberOfItems = 0;
+
+ function displayItemPicked(){
+    itemPicked.innerHTML = numberOfItems 
+    cartIconPicked.innerHTML = numberOfItems
+    itemPickedInCart.innerHTML = numberOfItems
+ }
+ 
+ function adder(){
+    numberOfItems++
+    displayItemPicked()
+ }
+ 
+ function substractor(){
+    numberOfItems--
+    displayItemPicked()
+ }
+   
+ additionButton.addEventListener("click", adder)
+  substractButton.addEventListener("click", substractor)
+ 
+
 
