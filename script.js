8a8f980e4837f91990eca06f95b211cf9c346a58
abujaipaul <abujaipaul1@gmail.com>
2025-10-14 16:08:16 +0100

    worked on the thumbnail picture focused when clicked and also worked on the add-to-cart button.

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const cartIcon = document.querySelector("#cart_icon_toggle")
 const cartBox = document.querySelector(".cart_box")
 const cartIndicatorItems = document.querySelector("#cart_indicator_items")
 const cartIndicatorEmpty = document.querySelector("#cart_indicator_empty")
+// buttons
 const deleteButton = document.querySelector("#dustbin_delete")
 const substractButton = document.querySelector("#substract_button")
 const additionButton = document.querySelector("#addition_button")
+const addToCartButton = document.querySelector("#add_to_cart_text")
 // variables tragetting the item picked span.
 const itemPicked = document.querySelectorAll("#item_picked")[2]
 const cartIconPicked = document.querySelectorAll("#item_picked")[0]
 const itemPickedInCart = document.querySelectorAll("#item_picked")[1]
+const cartBoxItems = document.querySelector(".cart_box_items")
+//thumbnails pictures
+const thumbnailPictures = document.querySelectorAll(".thumbnail_box")
+
 
 
  //to trigger the cart dropdown.
@@ -44,7 +50,22 @@ let numberOfItems = 0;
  }
    
  additionButton.addEventListener("click", adder)
-  substractButton.addEventListener("click", substractor)
+ substractButton.addEventListener("click", substractor)
  
+//to add items to cart box.
+ function addItems(){
+     cartBoxItems.append(cartIndicatorItems)
+      cartIndicatorEmpty.style = "display : none;"
+ }
 
+ addToCartButton.addEventListener("click", addItems)
 
+ //focus on thumbnail picture
+    thumbnailPictures.forEach((picture) => {
+        picture.addEventListener("click", () => {
+           picture.classList.toggle("thumbnail_focus")
+        })
+    })
+
+  
+ 
